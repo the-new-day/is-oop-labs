c65613412b9c05d5d8a7a43288ae3e0a9131b83f
the-new-day <balakineg977@gmail.com>
2025-11-12 18:38:54 +0300

    refactor: separate formatting from convertation
    - Add IMessageConverter for content transformation
    - Add IMessageFormatter for output writing
    - ConsoleMessageFormatter and FileMessageFormatter are now bridges
    between IMessageFormatter and IMessageConverter

--- src/Lab2/Entities/Formatting/ConsoleMessageFormatter.cs
+++ src/Lab2/Entities/Formatting/ConsoleMessageFormatter.cs
@@ -4,13 +4,20 @@ namespace Itmo.ObjectOrientedProgramming.Lab2.Entities.Formatting;
 
 public class ConsoleMessageFormatter : IMessageFormatter
 {
+    private readonly IMessageConverter _converter;
+
+    public ConsoleMessageFormatter(IMessageConverter converter)
+    {
+        _converter = converter;
+    }
+
     public void WriteHeader(string header)
     {
-        Console.WriteLine($"# {header}");
+        Console.Write(_converter.ConvertHeader(header));
     }
 
     public void WriteBody(string body)
     {
-        Console.WriteLine($"{body}");
+        Console.Write(_converter.ConvertBody(body));
     }
 }
--- src/Lab2/Entities/Formatting/FileMessageFormatter.cs
+++ src/Lab2/Entities/Formatting/FileMessageFormatter.cs
@@ -4,20 +4,23 @@ namespace Itmo.ObjectOrientedProgramming.Lab2.Entities.Formatting;
 
 public class FileMessageFormatter : IMessageFormatter
 {
+    private readonly IMessageConverter _converter;
+
     private readonly string _filePath;
 
-    public FileMessageFormatter(string filePath)
+    public FileMessageFormatter(IMessageConverter converter, string filePath)
     {
+        _converter = converter;
         _filePath = filePath;
     }
 
     public void WriteHeader(string header)
     {
-        File.AppendAllText(_filePath, $"# {header}\n");
+        File.AppendAllText(_filePath, _converter.ConvertHeader(header));
     }
 
     public void WriteBody(string body)
     {
-        File.AppendAllText(_filePath, $"{body}\n");
+        File.AppendAllText(_filePath, _converter.ConvertBody(body));
     }
 }
